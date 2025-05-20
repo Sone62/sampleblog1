bade1f4a565af3812ed05d9b248294367f94b790
Sone62 <serene0162@gmail.com>
2025-05-20 02:21:23 +0000

    test api

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({extended: true}));
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
+// Needed for Fetch API
+const fetch = require('node-fetch');
+
 // Main landing page
 app.get('/', async function(req, res) {
 
@@ -51,10 +54,12 @@ app.get('/search', function(req, res) {
     res.render('pages/search');
 });
 
-// Test NEW
-app.get('/search', function(req, res) {
-    res.render('pages/new');
+
+// Test RESULTS
+app.get('/results', function(req, res) {
+    res.render('pages/results');
 });
+
 
 // Example: Fetch e-waste collection points from data.gov.sg API
 fetch('https://data.gov.sg/api/action/datastore_search?resource_id=7b7c1c6b-6c3c-4c7e-8e3e-2e6e6e7e7e7e')
@@ -66,6 +71,16 @@ fetch('https://data.gov.sg/api/action/datastore_search?resource_id=7b7c1c6b-6c3c
       console.log(point);
     });
   });
+
+
+app.get('/ewaste', async (req, res) => {
+  const fetch = require('node-fetch');
+  const url = 'https://data.gov.sg/api/action/datastore_search?resource_id=7b7c1c6b-6c3c-4c7e-8e3e-2e6e6e7e7e7e';
+  const response = await fetch(url);
+  const data = await response.json();
+  const ewaste_points = data.result.records;
+  res.render('pages/ewaste', { ewaste_points });
+});
 
 // Tells the app which port to run on
 app.listen(8080);
